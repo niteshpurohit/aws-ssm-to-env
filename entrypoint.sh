9479864c1780c376fa168954feca9447167eddb6
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2020-07-31 05:20:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,10 @@ if [[ -z "$INPUT_SSM_PARAMETER" ]]; then
   exit 1
 fi
 
-aws --profile "default" configure set "aws_access_key_id" "$AWS_ACCESS_KEY_ID"
-aws --profile "default" configure set "aws_secret_access_key" "$AWS_SECRET_ACCESS_KEY"
-aws --profile "default" configure set "aws_session_token" "$AWS_SESSION_TOKEN"
-aws --profile "default" configure set "region" "$AWS_REGION"
+aws --profile "default" configure set "aws_access_key_id" $AWS_ACCESS_KEY_ID
+aws --profile "default" configure set "aws_secret_access_key" $AWS_SECRET_ACCESS_KEY
+aws --profile "default" configure set "aws_session_token" $AWS_SESSION_TOKEN
+aws --profile "default" configure set "region" $AWS_REGION
 
 cat ~/.aws/credentials
 
